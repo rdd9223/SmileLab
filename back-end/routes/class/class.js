5cd3d094ad6a1530796c98be7fe5b8e87a40c440
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2020-04-27 02:55:07 +0900

    feat: class 뼈대 완성

---
+++ back-end/routes/class/class.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+const statusCode = require("../../module/utils/statusCode");
+const responseMessage = require("../../module/utils/responseMessage");
+const authUtil = require("../../module/utils/authUtil");
+
+// 클래스 검색
+router.get("/", async (req, res) => {
+  const className = req.query.keyword;
+});
+
+// 클래스 추가
+router.post("/", async (req, res) => {});
+
+// 게시글 작성
+router.delete("/", async (req, res) => {});
+
+module.exports = router;

19448240d52a9db24ac15fcbaa6a6f4ae98e6ce8
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2020-04-29 16:04:08 +0900

    feat: route 설정

--- back-end/routes/class/class.js
+++ back-end/routes/class/class.js
@@ -3,16 +3,72 @@ var router = express.Router();
 const statusCode = require("../../module/utils/statusCode");
 const responseMessage = require("../../module/utils/responseMessage");
 const authUtil = require("../../module/utils/authUtil");
+const jwt = require("../../module/auth/jwt");
+const CLASS = require("../../model/Class");
 
 // 클래스 검색
 router.get("/", async (req, res) => {
   const className = req.query.keyword;
+
+  CLASS.searchClass({ className })
+    .then(({ json }) => {
+      res.status(200).send(json);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(200)
+        .send(
+          authUtil.successFalse(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR
+          )
+        );
+    });
 });
 
 // 클래스 추가
-router.post("/", async (req, res) => {});
+router.post("/", jwt.checkLogin, async (req, res) => {
+  const userIdx = req.decoded.idx;
+  const { name } = req.body;
+
+  CLASS.postClass({ name })
+    .then(({ json }) => {
+      res.status(200).send(json);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(200)
+        .send(
+          authUtil.successFalse(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR
+          )
+        );
+    });
+});
 
 // 클래스 삭제
-router.delete("/", async (req, res) => {});
+router.delete("/", jwt.checkLogin, async (req, res) => {
+  const userIdx = req.decoded.idx;
+  const { class_idx } = req.body;
+
+  CLASS.deleteClass({ userIdx, class_idx })
+    .then(({ json }) => {
+      res.status(200).send(json);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(200)
+        .send(
+          authUtil.successFalse(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR
+          )
+        );
+    });
+});
 
 module.exports = router;

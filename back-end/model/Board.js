360bf28fd42a3b1d2b00de101d075d594a78e0f1
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2020-04-29 14:52:53 +0900

    feat: 게시물 목록 조회

--- back-end/model/Board.js
+++ back-end/model/Board.js
@@ -1,10 +1,34 @@
 const statusCode = require("../module/utils/statusCode");
 const responseMessage = require("../module/utils/responseMessage");
 const authUtil = require("../module/utils/authUtil");
+const pool = require("../module/db/pool");
 
 const board = {
-  getBoardList: () => {
-    return new Promise(async (resolve, reject) => {});
+  getBoardList: ({ userIdx, page }) => {
+    return new Promise(async (resolve, reject) => {
+      const getUserTakeClassQuery = `SELECT class_idx FROM take WHERE student_idx = ${userIdx}`;
+      const getBoardListQuery = `SELECT * FROM board WHERE class_idx = (${getUserTakeClassQuery}) ORDER BY board_idx DESC LIMIT ${
+        page - 1
+      }, 10`;
+      const getBoardListResult = await pool.queryParam_Parse(getBoardListQuery);
+
+      if (getBoardListResult !== undefined) {
+        return resolve({
+          json: authUtil.successTrue(
+            statusCode.OK,
+            responseMessage.X_READ_SUCCESS(`${page}페이지 게시물`),
+            getBoardListResult
+          ),
+        });
+      } else {
+        return resolve({
+          json: authUtil.successFalse(
+            statusCode.BAD_REQUEST,
+            responseMessage.X_READ_FAIL(`${page}페이지 게시물`)
+          ),
+        });
+      }
+    });
   },
   postBoard: () => {
     return new Promise(async (resolve, reject) => {});
--- back-end/routes/board/board.js
+++ back-end/routes/board/board.js
@@ -3,14 +3,73 @@ var router = express.Router();
 const statusCode = require("../../module/utils/statusCode");
 const responseMessage = require("../../module/utils/responseMessage");
 const authUtil = require("../../module/utils/authUtil");
+const jwt = require("../../module/auth/jwt");
+const BOARD = require("../../model/Board");
 
 // 게시글 목록 조회
-router.get("/", async (req, res) => {});
+router.get("/:page", jwt.checkLogin, async (req, res) => {
+  const userIdx = req.decoded.idx;
+  const { page } = req.params;
+
+  BOARD.getBoardList({ userIdx, page })
+    .then(({ json }) => {
+      res.status(200).send(json);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(200)
+        .send(
+          authUtil.successFalse(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR
+          )
+        );
+    });
+});
 
 // 게시글 상세 조회
-router.get("/:boardIdx", async (req, res) => {});
+router.get("/:boardIdx", jwt.checkLogin, async (req, res) => {
+  const userIdx = req.decoded.idx;
+  const boardIdx = req.params;
+
+  BOARD.getBoardList({ userIdx, boardIdx })
+    .then(({ json }) => {
+      res.status(200).send(json);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(200)
+        .send(
+          authUtil.successFalse(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR
+          )
+        );
+    });
+});
 
 // 게시글 작성
-router.post("/", async (req, res) => {});
+router.post("/", jwt.checkLogin, async (req, res) => {
+  const userIdx = req.decoded.idx;
+  const { title, contents } = req.body;
+
+  BOARD.getBoardList({ userIdx, title, contents })
+    .then(({ json }) => {
+      res.status(200).send(json);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(200)
+        .send(
+          authUtil.successFalse(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR
+          )
+        );
+    });
+});
 
 module.exports = router;

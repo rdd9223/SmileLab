d27ea57c04b3454e49ab31b49b2aa2f30314af77
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2020-05-01 21:21:52 +0900

    feat: 메세지 전송

--- back-end/model/Message.js
+++ back-end/model/Message.js
@@ -1,15 +1,76 @@
 const statusCode = require("../module/utils/statusCode");
 const responseMessage = require("../module/utils/responseMessage");
 const authUtil = require("../module/utils/authUtil");
+const pool = require("../module/db/pool");
+const moment = require("moment");
+require("moment-timezone");
 
 const message = {
-  postMessage: () => {
+  postMessage: ({ userIdx, class_idx, contents, receiver }) => {
+    return new Promise(async (resolve, reject) => {
+      const date = moment().format("YYYY-MM-DD HH:mm:ss");
+      const ALL = 0;
+
+      if (!class_idx || !contents || !receiver) {
+        return resolve({
+          json: authUtil.successFalse(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE),
+        });
+      }
+
+      if (receiver == ALL) {
+        const postAllMessageQuery = `INSERT INTO message (from_user_idx, to_user_idx, date, contents) SELECT ?, student_idx, ?, ? FROM take WHERE class_idx = ${class_idx}`;
+        const postAllMessageResult = await pool.queryParam_Parse(postAllMessageQuery, [
+          class_idx,
+          date,
+          contents,
+        ]);
+
+        if (postAllMessageResult.affectedRows === 0) {
+          return resolve({
+            json: authUtil.successFalse(
+              statusCode.BAD_REQUEST,
+              responseMessage.X_CREATE_FAIL("전체 메세지")
+            ),
+          });
+        } else {
+          return resolve({
+            json: authUtil.successTrue(
+              statusCode.CREATED,
+              responseMessage.X_CREATE_SUCCESS("전체 메세지")
+            ),
+          });
+        }
+      } else {
+        const postMessageQuery = `INSERT INTO message (from_user_idx, to_user_idx, date, contents) VALUES (?, ?, ?, ?)`;
+        const postMessageResult = await pool.queryParam_Parse(postMessageQuery, [
+          class_idx,
+          receiver,
+          date,
+          contents,
+        ]);
+
+        if (postMessageResult.affectedRows === 0) {
+          return resolve({
+            json: authUtil.successFalse(
+              statusCode.BAD_REQUEST,
+              responseMessage.X_CREATE_FAIL("메세지")
+            ),
+          });
+        } else {
+          return resolve({
+            json: authUtil.successTrue(
+              statusCode.CREATED,
+              responseMessage.X_CREATE_SUCCESS("메세지")
+            ),
+          });
+        }
+      }
+    });
+  },
+  getMessageList: ({ userIdx }) => {
     return new Promise(async (resolve, reject) => {});
   },
-  getMessageList: () => {
-    return new Promise(async (resolve, reject) => {});
-  },
-  getMessageInfo: () => {
+  getMessageInfo: ({ userIdx, messageIdx }) => {
     return new Promise(async (resolve, reject) => {});
   },
 };
--- back-end/routes/message/message.js
+++ back-end/routes/message/message.js
@@ -3,16 +3,72 @@ var router = express.Router();
 const statusCode = require("../../module/utils/statusCode");
 const responseMessage = require("../../module/utils/responseMessage");
 const authUtil = require("../../module/utils/authUtil");
+const jwt = require("../../module/auth/jwt");
+const MESSAGE = require("../../model/Message");
 
 // 메세지 목록 조회
-router.get("/", async (req, res) => {
-  const className = req.query.keyword;
+router.get("/", jwt.checkLogin, async (req, res) => {
+  const userIdx = req.decoded.idx;
+
+  MESSAGE.getMessageList({ userIdx })
+    .then(({ json }) => {
+      res.status(200).send(json);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(200)
+        .send(
+          authUtil.successFalse(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR
+          )
+        );
+    });
 });
 
 // 메세지 내용 조회
-router.get("/:messageIdx", async (req, res) => {});
+router.get("/:messageIdx", jwt.checkLogin, async (req, res) => {
+  const userIdx = req.decoded.idx;
+  const { messageIdx } = req.params;
+
+  MESSAGE.getMessageList({ userIdx, messageIdx })
+    .then(({ json }) => {
+      res.status(200).send(json);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(200)
+        .send(
+          authUtil.successFalse(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR
+          )
+        );
+    });
+});
 
 // 메세지 전송
-router.post("/", async (req, res) => {});
+router.post("/", jwt.checkLogin, async (req, res) => {
+  const user_idx = req.decoded.idx;
+  const { class_idx, contents, receiver } = req.body;
+
+  MESSAGE.postMessage({ user_idx, class_idx, contents, receiver })
+    .then(({ json }) => {
+      res.status(200).send(json);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(200)
+        .send(
+          authUtil.successFalse(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR
+          )
+        );
+    });
+});
 
 module.exports = router;

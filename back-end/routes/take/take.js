d2db1595ef83cd833d712808d6c6ec71a2bf3675
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2020-05-01 22:07:13 +0900

    feat: 수강 루트 설정

--- back-end/routes/take/take.js
+++ back-end/routes/take/take.js
@@ -3,11 +3,50 @@ var router = express.Router();
 const statusCode = require("../../module/utils/statusCode");
 const responseMessage = require("../../module/utils/responseMessage");
 const authUtil = require("../../module/utils/authUtil");
+const jwt = require("../../module/auth/jwt");
+const TAKE = require("../../model/Take");
 
 // 클래스 수강하기
-router.post("/", async (req, res) => {});
+router.post("/", jwt.checkLogin, async (req, res) => {
+  const user_idx = req.decoded.idx;
+  const { class_idx } = req.body;
+
+  TAKE.takeClass({ user_idx, class_idx })
+    .then(({ json }) => {
+      res.status(200).send(json);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(200)
+        .send(
+          authUtil.successFalse(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR
+          )
+        );
+    });
+});
 
 // 수강클래스와 학생 전체 조회하기
-router.get("/", async (req, res) => {});
+router.get("/", jwt.checkLogin, async (req, res) => {
+  const user_idx = req.decoded.idx;
+
+  TAKE.getClassInfo({ user_idx })
+    .then(({ json }) => {
+      res.status(200).send(json);
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(200)
+        .send(
+          authUtil.successFalse(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR
+          )
+        );
+    });
+});
 
 module.exports = router;

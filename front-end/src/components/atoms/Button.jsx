a7e0e150e732cbd85c8713630850c3b8a1ae7d82
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2020-06-01 03:57:51 +0900

    refactor: props 변경

--- front-end/src/components/atoms/Button.jsx
+++ front-end/src/components/atoms/Button.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-const Button1 = (props) => {
-  const { style, variant, onClick, name, type } = props;
-
+const Button1 = ({ style, variant = "primary", onClick, name, type }) => {
   return (
     <Button style={style} variant={variant} onClick={onClick} type={type}>
       {name}

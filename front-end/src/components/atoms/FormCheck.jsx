001dedd047541b7fed8c53a4231a07c13ea76f3d
haaseungwoo <hansi100@naver.com>
2020-06-29 15:41:51 +0900

    fix: 회원가입 체크박스 onClick 에러 수정

--- front-end/src/components/atoms/FormCheck.jsx
+++ front-end/src/components/atoms/FormCheck.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Form } from "react-bootstrap";
 
-const FormCheck = ({ type, label, checked, name, id, onChange, disabled = false, onClick }) => {
-  return <Form.Check type={type} checked={checked} label={label} name={name} id={id} onChange={onChange} disabled={disabled} onClick={(id, checked) => onClick(id, checked)}/>;
+const FormCheck = ({ type, label, checked, name, id, onChange, disabled = false }) => {
+  return <Form.Check type={type} checked={checked} label={label} name={name} id={id} onChange={onChange} disabled={disabled} />;
 };
 
 export default FormCheck;

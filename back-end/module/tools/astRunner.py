f9a3ed12c5b926b4b7dc90b4b874d8d0e2b20186
haaseungwoo <hansi100@naver.com>
2020-07-01 15:40:58 +0900

    fix: ast runner 로직 개선

--- back-end/module/tools/astRunner.py
+++ back-end/module/tools/astRunner.py
@@ -16,6 +16,7 @@ def main():
 
 class Analyzer(ast.NodeVisitor):
     def __init__(self):
+        #내장함수
         self.innerFuncs = ["abs","all","any","chr","dir","divmod","zip","type","tuple","sum","str","sorted","round",
                             "range","pow","ord","open","oct","min","mas","map","list","len","isinstance","int","input",
                             "id","hex","filter","eval","enumerate"]
@@ -26,18 +27,17 @@ class Analyzer(ast.NodeVisitor):
                         "Constant": 0, "FunctionUse": [], "FunctionDef": [], "UnusedFunc": 0, "ParamOverThree" : 0,
                         "CountPrint": [], "PrintRepeat" : 0, "UsedInnerFunc" : []}
 
+    
     def visit_Assign(self, node):
         "할당정의 카운터 ex) a=5"
         self.stats["Assign"] += 1
-        if isinstance(node.value, ast.List):
-            self.stats["list"] += 1
-        if isinstance(node.value, ast.Tuple):
-            self.stats["tuple"] += 1
+            
         if isinstance(node.value, ast.Constant):
             if isinstance(node.value.value, str):
                 self.stats["Str"] += 1
             if isinstance(node.value.value, int):
                 self.stats["num"] += 1
+
         #자기자신에 대한 사칙연산
         if len(node.targets) == 1 and isinstance(node.targets[0], ast.Name) :
             if isinstance(node.value, ast.BinOp):
@@ -45,11 +45,6 @@ class Analyzer(ast.NodeVisitor):
                     self.stats["SelfOp"] += 1
                 if isinstance(node.value.right, ast.Name) and (node.targets[0].id is node.value.right.id):
                     self.stats["SelfOp"] += 1
-                    
-        
-        # if isinstance(node.targets[0], ast.Tuple):
-        #     for i in range(2):
-        #         self.visit_Name(node.targets[0].elts[i])
         self.generic_visit(node)
 
     def visit_Constant(self, node):
@@ -59,65 +54,59 @@ class Analyzer(ast.NodeVisitor):
 
     def visit_Tuple(self, node):
         "튜플 카운터"
-        # self.stats["Name"].append(node)
+        self.stats["tuple"] += 1
+        self.generic_visit(node)
+    
+    def visit_List(self, node):
+        "리스트 카운터"
+        self.stats["list"] += 1
         self.generic_visit(node)
 
     def visit_BinOp(self, node):
         "연산자 카운터 ex) a=3*6"
         self.stats["BinOp"] += 1
         self.generic_visit(node)
-    
+
+    def visit_Attribute(self, node):
+        "Attribute 카운터"
+
     def visit_Call(self, node):
         "호출 카운터"
+        length = 0
+        name = ''
+        if isinstance(node.func, ast.Name):
+            length = len(node.args)
+            name = str(length)+node.func.id
+            if node.func.id in self.innerFuncs and node.func.id not in self.stats["UsedInnerFunc"]:
+                self.stats["UsedInnerFunc"].append(node.func.id)
         if isinstance(node.func, ast.Attribute):
             length = len(node.args) + 1
             name = str(length)+node.func.attr
-            if name in self.stats["FunctionUse"]:
-                pass
-            else:
-                if name in self.stats["FunctionDef"]:
-                    self.stats["FunctionUse"].append(name)
-
             if node.func.attr in self.innerFuncs :
                 self.stats["UsedInnerFunc"] += 1
+        if name in self.stats["FunctionUse"]:
+            pass
         else:
-            length = len(node.args)
-            name = str(length)+node.func.id
-            if name in self.stats["FunctionUse"]:
-                pass
-            else:
-                if name in self.stats["FunctionDef"]:
-                    self.stats["FunctionUse"].append(name)
-            if node.func.id in self.innerFuncs and node.func.id not in self.stats["UsedInnerFunc"]:
-                self.stats["UsedInnerFunc"].append(node.func.id)
+            if name in self.stats["FunctionDef"]:
+                self.stats["FunctionUse"].append(name)
+       
+        
+        self.generic_visit(node)
+        
 
     def visit_Expr(self, node):
         "표현식에 대한 방문 정의 ex)3*7+5"
-        self.stats["Expr"] += 1   
-            #if isinstance(node.value.func, ast.Name):
-            #    if node.value.func.id == 'print':
-            #        if node.value.args:
-            #            cursor = None
-            #            for item in self.stats["CountPrint"] :
-            #                #print(item)
-            #                if item[node.value.args[0].value] :
-            #                    item[node.value.args[0].value] += 1
-            #                    cursor = item[node.value.args[0].value]
-            #                    if item[node.value.args[0].value] > 2 :
-            #                        self.stats["PrintRepeat"] += 1
-            # 
-            #            if not isinstance(node.value.args[0], ast.Call) and cursor is None :
-            #                if not isinstance(node.value.args[0], ast.IfExp) :
-            #                    self.stats["CountPrint"].append({ str(node.value.args[0].value) : 1})
-                                #print(self.stats["CountPrint"])
-        # self.generic_visit(node)
+        self.stats["Expr"] += 1
+        self.generic_visit(node)
 
     def visit_Name(self, node):
+        #print(node.ctx)
         "변수에 값을 할당하기 위한 정의. ex) a=5값 5를 보유하는 변수 a를 말함"
-        if node.id in self.stats["Name"]:
-            pass
-        else:
-            self.stats["Name"].append(node.id)
+        if isinstance(node.ctx, ast.Store):
+            if node.id in self.stats["Name"]:
+                pass
+            else:
+                self.stats["Name"].append(node.id)
  
     def visit_AugAssign(self, node):
         "단항연산자 카운터"
@@ -127,10 +116,10 @@ class Analyzer(ast.NodeVisitor):
     def visit_If(self, node):
         "조건문 카운터"
         for element in node.body:
-            if isinstance(element, ast.If) :
+            if isinstance(element, ast.If):
                 if len(element.orelse) != 0 :
                     self.stats["UniqIf"] += 1
-        if len(node.orelse) == 0 :
+        if len(node.orelse) == 0:
             self.stats["If"] += 1
             self.generic_visit(node)
         else:
@@ -140,7 +129,6 @@ class Analyzer(ast.NodeVisitor):
                 self.stats["ElseIf"] += 1
                 self.generic_visit(node)
         
-
     def visit_For(self, node):
         "for 카운터"
         self.stats["For"] += 1

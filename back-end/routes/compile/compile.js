92d99e3dfd63c592326f74ad66c8834c49048c07
rdd9223 <rdd9223@gmail.com>
2020-07-09 09:29:38 +0900

    fix: 컴파일 속도 올리기

--- back-end/routes/compile/compile.js
+++ back-end/routes/compile/compile.js
@@ -9,18 +9,18 @@ const resMessage = require("../../module/utils/responseMessage");
 
 // 컴파일 하기
 router.post("/", async (req, res) => {
-  let sourcePath = path.join(__dirname, `../source/${req.body.userId}`);
-  console.log(sourcePath);
+  let sourcePath = path.join(__dirname, `../../source/${req.body.userId}`);
 
-  if (!fs.existsSync(sourcePath, {recursive: true })) {
-    fs.mkdirSync(sourcePath, {recursive: true });
+  if (!fs.existsSync(sourcePath, { recursive: true })) {
+    fs.mkdirSync(sourcePath, { recursive: true });
   }
 
   fs.writeFileSync(path.join(sourcePath, "Main.py"), req.body.source);
   sourcePath = sourcePath.replace(/\\/gi, "/");
 
-  const docker = await exec(
-    `docker run --rm -v ${sourcePath}:/usr/src -w /usr/src python:3 python Main.py`,
+  const runner = await exec(
+    // `docker run --rm -v ${sourcePath}:/usr/src -w /usr/src python:3 python Main.py`
+    `python3 ${sourcePath}/Main.py`,
     (err, out, stderr) => {
       if (out) {
         res.status(200).send(authUtil.successTrue(statusCode.OK, "컴파일 성공", out));
@@ -33,15 +33,15 @@ router.post("/", async (req, res) => {
 
 // 컴파일 결과 확인하기
 router.post("/result", async (req, res) => {
-  let sourcePath = path.join(__dirname, `../source/${req.body.userId}`);
-  let testPath = path.join(__dirname, `../../module/tools/`);
+  let sourcePath = path.join(__dirname, `../../source/${req.body.userId}`);
+  let testPath = path.join(__dirname, `../../module/tools`);
   sourcePath = sourcePath.replace(/\\/gi, "/");
   testPath = testPath.replace(/\\/gi, "/");
 
   const docker = await exec(
     `docker run --rm -v ${sourcePath}:/usr/src/src  \ -v ${testPath}:/usr/src/test -w /usr/src python:3 python test/astRunner.py ../src/Main.py`,
+    // `python3 ${testPath}/astRunner.py ${sourcePath}/Main.py`,
     (err, out, stderr) => {
-      console.log(out)
       if (out) {
         res.status(200).send(authUtil.successTrue(statusCode.OK, "컴파일 성공", out));
       } else {
@@ -49,12 +49,6 @@ router.post("/result", async (req, res) => {
       }
     }
   );
-/*
-  if (out) {
-    res.status(200).send(authUtil.successTrue(statusCode.OK, "컴파일 성공", out));
-  } else {
-    res.status(200).send(authUtil.successTrue(statusCode.BAD_REQUEST, "컴파일 실패", stderr));
-  }*/
 });
 
 module.exports = router;

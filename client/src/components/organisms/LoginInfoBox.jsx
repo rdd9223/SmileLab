a5e14dbdef60930b51779f4617c85219e1d8cb9f
haaseungwoo <hansi100@naver.com>
2020-08-16 20:43:50 +0900

    fix: 모든 유저가 메세지 보낼 수 있도록 수정

--- client/src/components/organisms/LoginInfoBox.jsx
+++ client/src/components/organisms/LoginInfoBox.jsx
@@ -12,72 +12,41 @@ const  LoginInfoBox = ({ userType }) => {
     window.location.reload(true);
   };
   
-  if (userType === 2) {
-    return (
-      <Container
-        style={{
-          textAlign: "center",
-          paddingTop: "200px",
-          paddingBottom: "200px",
-        }}
-      >
-        <h6>로그인 되었습니다</h6>
-        <h6>상단에서 원하는 메뉴를 선택하세요.</h6>
-        <Row style={{ marginTop: "100px" }}>
-          <Col>
-            <Link to="/">
-              <h1 onClick={() => logout()}>
-                <FiLogOut />
-              </h1>
-            </Link>
-          </Col>
-          <Col>
-            <Link to={"/mypage"}>
-              <h1>
-                <FiUser />
-              </h1>
-            </Link>
-          </Col>
-          <Col>
-            <Link to={"/message"}>
-              <h1>
-                <FiMail />
-              </h1>
-            </Link>
-          </Col>
-        </Row>
-      </Container>
-    );
-  } else {
-    return (
-      <Container
-        style={{
-          textAlign: "center",
-          paddingTop: "200px",
-          paddingBottom: "200px",
-        }}
-      >
-        <h6>로그인 되었습니다</h6>
-        <h6>상단에서 원하는 메뉴를 선택하세요.</h6>
-        <Row style={{ marginTop: "100px" }}>
-          <Col>
-            <Link to="/">
-              <h1 onClick={() => logout()}>
-                <FiLogOut />
-              </h1>
-            </Link>
-          </Col>
-          <Col>
-            <Link to={"/mypage"}>
-              <h1>
-                <FiUser />
-              </h1>
-            </Link>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+  return (
+    <Container
+      style={{
+        textAlign: "center",
+        paddingTop: "200px",
+        paddingBottom: "200px",
+      }}
+    >
+      <h6>로그인 되었습니다</h6>
+      <h6>상단에서 원하는 메뉴를 선택하세요.</h6>
+      <Row style={{ marginTop: "100px" }}>
+        <Col>
+          <Link to="/">
+            <h1 onClick={() => logout()}>
+              <FiLogOut />
+            </h1>
+          </Link>
+        </Col>
+        <Col>
+          <Link to={"/mypage"}>
+            <h1>
+              <FiUser />
+            </h1>
+          </Link>
+        </Col>
+        <Col>
+          <Link to={"/message"}>
+            <h1>
+              <FiMail />
+            </h1>
+          </Link>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default LoginInfoBox;

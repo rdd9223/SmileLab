014c18cf3dc69000d5e67a7aabc181ba83e2d546
hanseungwoo <seungwoo.han@kscy.kr>
2021-03-11 19:21:06 +0900

    fix: 드래그앤드롭에서 아무것도 없을 시 에러 뜨는 현상 수정

--- client/src/components/organisms/DragAndDrop.jsx
+++ client/src/components/organisms/DragAndDrop.jsx
@@ -27,7 +27,7 @@ const DragAndDrop = (props) => {
   //console.log(JSON.parse(window.localStorage.getItem("text_drag")).children[0].children)
 
   React.useEffect(() => {
-    if(images.length === 0 && JSON.parse(window.localStorage.getItem("text_drag")).children[0].children != null){
+    if(images.length === 0 && JSON.parse(window.localStorage.getItem("text_drag")) != null){
       const tempImages = []
       const savedImages =  JSON.parse(window.localStorage.getItem("text_drag")).children[0].children
       for(const savedImage of savedImages){
